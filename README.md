85198d76919826741f2495d1625838a74ab283c1
tom <tsadowski93@gmail.com>
2024-04-23 10:35:39 -0400

    saving progress

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ There are 3 modes: Alpha, Mouse, and Number.
 
 ![alpha_mode_graph](https://github.com/tomsadowski/combo_town/blob/master/documentation_assets/alpha_graph.jpg)  
 
-Note: Capslock will also end if the mode is changed to Mouse or Number, or if Ctrl, Alt, OS, or Escape is pressed.   
-
 ---  
 
 ![mouse_mode_graph](https://github.com/tomsadowski/combo_town/blob/master/documentation_assets/mouse_graph.jpg)  
@@ -24,15 +22,10 @@ Because some combos share keys, the following list of combos is shown across mul
 
 ![combos_1_of_3](https://github.com/tomsadowski/combo_town/blob/master/documentation_assets/combos_1.png) 
 
-![combos_3_of_3](https://github.com/tomsadowski/combo_town/blob/master/documentation_assets/combos_3.png)  
-
 ![combos_2_of_3](https://github.com/tomsadowski/combo_town/blob/master/documentation_assets/combos_2.png)  
+Note: Capslock will also end if the mode is changed to Mouse or Number, or if Ctrl, Alt, OS, or Escape is pressed.   
 
-The two combos in the top row labelled L and R represent Left `mod-builder` and Right `mod-builder`.    
-
-## Mod-Builder  
-
-Pressing any key while a `mod-builder` combo is held down will place that keycode in a queue stored in the keyboard's memory. If there are keys in the queue and you press a key without holding a `mod-builder` combo, that keycode along with all keycodes in the queue will be sent until you release the key. This remains true even if the key you press is already in the queue. However, tapping a `mod-builder` combo without adding a keycode will clear the queue, note that a keycode will not be added if it is already in the queue. The maximum queue size is 8, adding an 8th key will result in all keycodes being sent.  
+![combos_3_of_3](https://github.com/tomsadowski/combo_town/blob/master/documentation_assets/combos_3.png)  
 
 ## Bonus: Game Modes   
 
--- keymap.c
+++ keymap.c
@@ -1,5 +1,5 @@
 // tomsadowski
-// keymap for ferris sweep
+// keymap for ferris-like keyboards
 
 #include "action_layer.h"
 #include "process_combo.h"
@@ -8,7 +8,7 @@
 #include "keycodes.h"
 #include "quantum_keycodes.h"
 
-enum my_keycodes {CAPS_ON = SAFE_RANGE, CAPS_OFF};
+enum my_keycodes {CAPS_ON = SAFE_RANGE};
 
 // DATA: LAYERS
 
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NMBR_LAYER] = LAYOUT_split_3x5_2( // NUMBER: digits, navigation keys, and symbols
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_LBRC, KC_RBRC, KC_BSLS, KC_SLSH, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
-        _______, _______, _______, KC_GRV,  _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+        _______, KC_QUOT, KC_SCLN, KC_GRV,  _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
                                    _______, _______,         LT(MO_ALP_LYR, KC_SPC), _______),
     [MOUS_LAYER] = LAYOUT_split_3x5_2( // MOUSE: Mouse and function keys
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
@@ -53,12 +53,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MO_MNM_LYR] = LAYOUT_split_3x5_2( // MOMENTARY NUMBER: accessed from MOMENTARY MOUSE ALPHA
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_LBRC, KC_RBRC, KC_BSLS, KC_SLSH, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
-        _______, _______, _______, KC_GRV,  _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+        _______, KC_QUOT, KC_SCLN, KC_GRV,  _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
                                    _______, LT(MO_MAL_LYR, KC_SPC),          _______, _______),
     [MO_NMB_LYR] = LAYOUT_split_3x5_2( // MOMENTARY NUMBER: accessed from ALPHA
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_LBRC, KC_RBRC, KC_BSLS, KC_SLSH, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
-        _______, _______, _______, KC_GRV,  _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+        _______, KC_QUOT, KC_SCLN, KC_GRV,  _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
                                    _______, KC_SPC,          _______, _______),
     [MO_MSE_LYR] = LAYOUT_split_3x5_2( // MOMENTARY MOUSE: accessed from ALPHA
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
@@ -79,13 +79,15 @@ enum combos {
                     CTL_COMBO_L,                               CTL_COMBO_R,
                     G3D_COMBO_L,                               MSE_COMBO_R,
   /*   |___|   |___|   |___|   |XXX|   |XXX|      |XXX|   |XXX|   |___|   |___|   |___|   */
-                    CON_COMBO_L,                               COF_COMBO_R,
+                    PSC_COMBO_L,                               PSC_COMBO_R,
                     ESC_COMBO_L,                               ENT_COMBO_R,
                     GUI_COMBO_L,                               GUI_COMBO_R,
   /*   |XXX|   |___|   |___|   |___|   |XXX|      |XXX|   |___|   |___|   |___|   |XXX|   */
-                    PSC_COMBO_L,                               PSC_COMBO_R,
+                    BSP_COMBO_L,                               DEL_COMBO_R,
                     QTE_COMBO_L,                               SCL_COMBO_R,
                                                                NMB_COMBO_R,
+  /*   |___|   |222|   |222|   |___|   |___|               |___|   |___|   |222|   |222|   |___|   */
+                                        CAP_COMBO_M,
 };
 
 // DATA: COMBO KEYS
@@ -98,11 +100,11 @@ const uint16_t PROGMEM alt_combo_l[] = {KC_X,   KC_B,     COMBO_END};
 const uint16_t PROGMEM ctl_combo_l[] = {KC_A,   KC_T,     COMBO_END};
 const uint16_t PROGMEM g3d_combo_l[] = {KC_V,   KC_D,     COMBO_END};
 //                                          L:  -  -  -  X  X
-const uint16_t PROGMEM con_combo_l[] = {KC_B,   KC_Q,     COMBO_END};
+const uint16_t PROGMEM psc_combo_l[] = {KC_B,   KC_Q,     COMBO_END};
 const uint16_t PROGMEM esc_combo_l[] = {KC_T,   KC_COMMA, COMBO_END};
 const uint16_t PROGMEM gui_combo_l[] = {KC_D,   KC_MINS,  COMBO_END};
 //                                          L:  X  -  -  -  X
-const uint16_t PROGMEM psc_combo_l[] = {KC_X,   KC_Q,     COMBO_END};
+const uint16_t PROGMEM bsp_combo_l[] = {KC_X,   KC_Q,     COMBO_END};
 const uint16_t PROGMEM qte_combo_l[] = {KC_A,   KC_COMMA, COMBO_END};
 //                                          R:  -  -  X  X  -
 const uint16_t PROGMEM tab_combo_r[] = {KC_W,   KC_P,     COMBO_END};
@@ -113,13 +115,15 @@ const uint16_t PROGMEM alt_combo_r[] = {KC_Y,   KC_J,     COMBO_END};
 const uint16_t PROGMEM ctl_combo_r[] = {KC_E,   KC_N,     COMBO_END};
 const uint16_t PROGMEM mse_combo_r[] = {KC_U,   KC_K,     COMBO_END};
 //                                          R:  X  X  -  -  -
-const uint16_t PROGMEM cof_combo_r[] = {KC_Z,   KC_Y,     COMBO_END};
+const uint16_t PROGMEM psc_combo_r[] = {KC_Z,   KC_Y,     COMBO_END};
 const uint16_t PROGMEM ent_combo_r[] = {KC_DOT, KC_E,     COMBO_END};
 const uint16_t PROGMEM gui_combo_r[] = {KC_EQL, KC_U,     COMBO_END};
 //                                          R:  X  -  -  -  X
-const uint16_t PROGMEM psc_combo_r[] = {KC_Z,   KC_J,     COMBO_END};
+const uint16_t PROGMEM del_combo_r[] = {KC_Z,   KC_J,     COMBO_END};
 const uint16_t PROGMEM scl_combo_r[] = {KC_DOT, KC_N,     COMBO_END};
 const uint16_t PROGMEM nmb_combo_r[] = {KC_EQL, KC_K,     COMBO_END};
+//                                          M: - 2 2 - -   - - 2 2 -
+const uint16_t PROGMEM cap_combo_m[] = {KC_S, KC_R, KC_I, KC_O, COMBO_END};
 
 // DATA: COMBO ASSIGNMENTS
 
@@ -133,13 +137,15 @@ combo_t key_combos[] = {
   [CTL_COMBO_L] = COMBO(ctl_combo_l, KC_LCTL),         [CTL_COMBO_R] = COMBO(ctl_combo_r, KC_RCTL),
   [G3D_COMBO_L] = COMBO(g3d_combo_l, TO(GM3D_LAYER)),  [MSE_COMBO_R] = COMBO(mse_combo_r, TO(MOUS_LAYER)),
   /*   |___|   |___|   |___|   |XXX|   |XXX|               |XXX|   |XXX|   |___|   |___|   |___|   */
-  [CON_COMBO_L] = COMBO(con_combo_l, CAPS_ON),         [COF_COMBO_R] = COMBO(cof_combo_r, CAPS_OFF),
+  [PSC_COMBO_L] = COMBO(psc_combo_l, KC_PSCR),         [PSC_COMBO_R] = COMBO(psc_combo_r, KC_PSCR),
   [ESC_COMBO_L] = COMBO(esc_combo_l, KC_ESC),          [ENT_COMBO_R] = COMBO(ent_combo_r, KC_ENT),
   [GUI_COMBO_L] = COMBO(gui_combo_l, KC_LGUI),         [GUI_COMBO_R] = COMBO(gui_combo_r, KC_RGUI),
   /*   |XXX|   |___|   |___|   |___|   |XXX|               |XXX|   |___|   |___|   |___|   |XXX|   */
-  [PSC_COMBO_L] = COMBO(psc_combo_l, KC_PSCR),         [PSC_COMBO_R] = COMBO(psc_combo_r, KC_PSCR),
+  [BSP_COMBO_L] = COMBO(bsp_combo_l, KC_BSPC),         [DEL_COMBO_R] = COMBO(del_combo_r, KC_DEL),
   [QTE_COMBO_L] = COMBO(qte_combo_l, KC_QUOTE),        [SCL_COMBO_R] = COMBO(scl_combo_r, KC_SCLN),
                                                        [NMB_COMBO_R] = COMBO(nmb_combo_r, TO(NMBR_LAYER)),
+  /*   |___|   |222|   |222|   |___|   |___|               |___|   |___|   |222|   |222|   |___|   */
+                             [CAP_COMBO_M] = COMBO(cap_combo_m, CAPS_ON),
 };
 
 // FUNCTION: COMBO
@@ -159,6 +165,7 @@ bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode
             default: return false;
         }
     }
+    if (combo_index == ALP_COMBO_R) caps_word_off();
     return true;
 }
 // Shorten combo term when in game layers
@@ -175,7 +182,7 @@ uint16_t get_combo_term(uint16_t index, combo_t *combo) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
         case GM2D_LAYER ... MOUS_LAYER:
-            if (is_caps_word_on()) caps_word_off(); break;
+            caps_word_off(); break;
         default: break;
     }
   return state;
@@ -201,15 +208,12 @@ bool caps_word_press_user(uint16_t keycode) {
     }
 }
 
-// FUNCTION: CAPS, MOD-BUILDER
-// Handles CAPS_ON/OFF and updates mbq state. State of mbq alters default keycode processing
+// FUNCTION: CAPS
+// Handles CAPS_ON/OFF
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case CAPS_ON:
-            if (!is_caps_word_on()) caps_word_on();
-            return false;
-        case CAPS_OFF:
-            if (is_caps_word_on()) caps_word_off();
+            caps_word_on();
             return false;
         default: return true;
     }

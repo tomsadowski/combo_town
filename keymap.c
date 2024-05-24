5f2cfc5db08162294103dbde9903f113bb1a834c
tom <tsadowski93@gmail.com>
2024-05-24 09:44:34 -0400

    chris hedges would approve of my keymaps

--- keymap.c
+++ keymap.c
@@ -21,64 +21,64 @@ typedef struct {
 key_trap;
 static key_trap trap = {CLOSED, {0}, KC_NO, 0};
 
-enum layers {ALPHA_LAYER, GM2D_LAYER, ANIMAL_WELL_LOL, GM3D_LAYER, NMBR_LAYER, MOUS_LAYER,
+enum layers {ALPHA_LAYER, GAME_LAYER, ANIMAL_WELL_LOL, LNAV_LAYER, NMBR_LAYER, MOUS_LAYER,
              MO_ALP_LYR, MO_MAL_LYR, MO_MNM_LYR, MO_NMB_LYR, MO_MSE_LYR};
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [ALPHA_LAYER] = LAYOUT_split_3x5_2( // ALPHA: Alphabet, punctuation, editing
         KC_X,    KC_C,    KC_H,    KC_B,    KC_Q,            KC_Z,    KC_Y,    KC_W,    KC_P,    KC_J,
         KC_A,    KC_S,    KC_R,    KC_T,    KC_COMMA,        KC_DOT,  KC_E,    KC_I,    KC_O,    KC_N,
         KC_V,    KC_F,    KC_L,    KC_D,    KC_MINS,         KC_EQL,  KC_U,    KC_M,    KC_G,    KC_K,
-                 KC_BSPC, LT(MO_MSE_LYR, KC_SPC),            LT(MO_NMB_LYR, KC_SPC), KC_DEL),
-    [GM2D_LAYER] = LAYOUT_split_3x5_2( // GAME 2D: Base sans combos and hold-tap features
+                                  KC_BSPC, LT(MO_MSE_LYR, KC_SPC),       LT(MO_NMB_LYR, KC_SPC),            KC_DEL),
+    [GAME_LAYER] = LAYOUT_split_3x5_2( // GAME: Base sans combos and hold-tap features
         _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
-                                   _______, KC_SPC,          KC_0,    _______),
+                                               _______, KC_SPC,          KC_0,    _______),
     [ANIMAL_WELL_LOL] = LAYOUT_split_3x5_2( // animal well lol
-        KC_A, KC_W, KC_S,  KC_D, KC_F,                       KC_G,    KC_H, KC_J, KC_K, KC_L,
-        KC_LEFT, KC_UP, KC_DOWN,   KC_RIGHT, _______,        _______, KC_SPC,    KC_Z,    KC_X,  KC_C,
-        KC_4, KC_5, KC_6, KC_7,  KC_8,        KC_9, KC_0,   KC_1,    KC_2,  KC_3,
-                                   _______, KC_LSFT,                        KC_V,    _______),
-    [GM3D_LAYER] = LAYOUT_split_3x5_2( // GAME 3D: Game 2d but with mouse
-        _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,         _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-        _______, _______, _______, _______, _______,         _______, _______, KC_WH_D, KC_WH_U, _______,
-                                   _______, KC_SPC,          KC_BTN1, KC_BTN2),
+        KC_A,    KC_W,   KC_S,     KC_D,    KC_F,            KC_G,    KC_H,     KC_J,   KC_K,    KC_L,
+        KC_LEFT, KC_UP,  KC_DOWN,  KC_RGHT, _______,         _______, KC_SPC,   KC_Z,   KC_X,    KC_C,
+        KC_4,    KC_5,   KC_6,     KC_7,    KC_8,            KC_9,    KC_0,     KC_1,   KC_2,    KC_3,
+                                               _______, KC_LSFT,         KC_V,    _______),
+    [LNAV_LAYER] = LAYOUT_split_3x5_2( // LEFT NAVIGATION: Navigation on the left, useful when flashing
+        KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______,         _______, _______, _______, _______, _______,
+        KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,         _______, _______, _______, _______, _______,
+        _______, KC_WH_U, KC_WH_D, _______, _______,         _______, _______, _______, _______, _______,
+                                               KC_BTN1, KC_BTN2,         KC_SPC,  _______),
     [NMBR_LAYER] = LAYOUT_split_3x5_2( // NUMBER: digits, navigation keys, and symbols
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_BSLS, KC_SLSH, KC_QUOT, KC_SCLN, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
+        KC_BSLS, KC_SLSH, KC_QUOT, KC_SCLN, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
         _______, KC_GRV,  KC_LBRC, KC_RBRC, _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-                                   _______, _______,         LT(MO_ALP_LYR, KC_SPC), _______),
+                                               _______, _______,         LT(MO_ALP_LYR, KC_SPC),            _______),
     [MOUS_LAYER] = LAYOUT_split_3x5_2( // MOUSE: Mouse and function keys
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
         KC_F11,  KC_F12,  KC_ACL1, KC_ACL0, _______,         _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
         _______, _______, _______, KC_ACL2, _______,         _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
-                 _______, LT(MO_MAL_LYR, KC_SPC),            KC_BTN1, KC_BTN2),
+                                  _______, LT(MO_MAL_LYR, KC_SPC),       KC_BTN1, KC_BTN2),
     [MO_ALP_LYR] = LAYOUT_split_3x5_2( // MOMENTARY ALPHA: Accessed from NUMBER
         KC_X,   KC_C,     KC_H,    KC_B,    KC_Q,            KC_Z,    KC_Y,    KC_W,    KC_P,    KC_J,
         KC_A,   KC_S,     KC_R,    KC_T,    _______,         _______, KC_E,    KC_I,    KC_O,    KC_N,
         KC_V,   KC_F,     KC_L,    KC_D,    _______,         _______, KC_U,    KC_M,    KC_G,    KC_K,
-                                   _______, KC_SPC,          _______, _______),
+                                               _______, KC_SPC,          _______, _______),
     [MO_MAL_LYR] = LAYOUT_split_3x5_2( // MOMENTARY MOUSE ALPHA: accessed from MOUSE
         KC_X,   KC_C,     KC_H,    KC_B,    KC_Q,            KC_Z,    KC_Y,    KC_W,    KC_P,     KC_J,
         KC_A,   KC_S,     KC_R,    KC_T,    _______,         _______, KC_E,    KC_I,    KC_O,     KC_N,
         KC_V,   KC_F,     KC_L,    KC_D,    _______,         _______, KC_U,    KC_M,    KC_G,     KC_K,
-                                   _______, _______,         LT(MO_MNM_LYR, KC_SPC), KC_DEL),
+                                               _______, _______,         LT(MO_MNM_LYR, KC_SPC),            KC_DEL),
     [MO_MNM_LYR] = LAYOUT_split_3x5_2( // MOMENTARY MOUSE NUMBER: accessed from MOMENTARY MOUSE ALPHA
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_BSLS, KC_SLSH, KC_QUOT, KC_SCLN, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
         _______, KC_GRV,  KC_LBRC, KC_RBRC, _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-                                   _______, LT(MO_MAL_LYR, KC_SPC),          _______, _______),
+                                  _______, LT(MO_MAL_LYR, KC_SPC),       _______, _______),
     [MO_NMB_LYR] = LAYOUT_split_3x5_2( // MOMENTARY NUMBER: accessed from ALPHA
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_BSLS, KC_SLSH, KC_QUOT, KC_SCLN, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
         _______, KC_GRV,  KC_LBRC, KC_RBRC, _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-                                   _______, KC_SPC,          _______, _______),
+                                               _______, KC_SPC,          _______, _______),
     [MO_MSE_LYR] = LAYOUT_split_3x5_2( // MOMENTARY MOUSE: accessed from ALPHA
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
         KC_F11,  KC_F12,  KC_ACL1, KC_ACL0, _______,         _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
         _______, _______, KC_SCLN, KC_ACL2, _______,         _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
-                                   _______, _______,         KC_BTN1, KC_BTN2),
+                                               _______, _______,         KC_BTN1, KC_BTN2),
 };
 
 enum combos {
@@ -89,14 +89,14 @@ enum combos {
   /*   |XXX|   |___|   |___|   |XXX|   |___|      |___|   |XXX|   |___|   |___|   |XXX|   */
                     ALT_COMBO_L,                               ALT_COMBO_R,
                     CTL_COMBO_L,                               CTL_COMBO_R,
-                    G3D_COMBO_L,                               MSE_COMBO_R,
+                    LNV_COMBO_L,                               MSE_COMBO_R,
   /*   |___|   |___|   |___|   |XXX|   |XXX|      |XXX|   |XXX|   |___|   |___|   |___|   */
                     PSC_COMBO_L,                               PSC_COMBO_R,
                     ESC_COMBO_L,                               ENT_COMBO_R,
                     GUI_COMBO_L,                               GUI_COMBO_R,
   /*   |XXX|   |___|   |___|   |___|   |XXX|      |XXX|   |___|   |___|   |___|   |XXX|   */
                     KTP_COMBO_L,                               KTP_COMBO_R,
-                    G2D_COMBO_L,                               NMB_COMBO_R,
+                    GME_COMBO_L,                               NMB_COMBO_R,
                     ANI_COMBO_L,
 };
 
@@ -107,14 +107,14 @@ const uint16_t PROGMEM cap_combo_l[] = {KC_F,   KC_L,     COMBO_END};
 //                                          L:  X  -  -  X  -
 const uint16_t PROGMEM alt_combo_l[] = {KC_X,   KC_B,     COMBO_END};
 const uint16_t PROGMEM ctl_combo_l[] = {KC_A,   KC_T,     COMBO_END};
-const uint16_t PROGMEM g3d_combo_l[] = {KC_V,   KC_D,     COMBO_END};
+const uint16_t PROGMEM lnv_combo_l[] = {KC_V,   KC_D,     COMBO_END};
 //                                          L:  -  -  -  X  X
 const uint16_t PROGMEM psc_combo_l[] = {KC_B,   KC_Q,     COMBO_END};
 const uint16_t PROGMEM esc_combo_l[] = {KC_T,   KC_COMMA, COMBO_END};
 const uint16_t PROGMEM gui_combo_l[] = {KC_D,   KC_MINS,  COMBO_END};
 //                                          L:  X  -  -  -  X
 const uint16_t PROGMEM ktp_combo_l[] = {KC_X,   KC_Q,     COMBO_END};
-const uint16_t PROGMEM g2d_combo_l[] = {KC_A,   KC_COMM,  COMBO_END};
+const uint16_t PROGMEM gme_combo_l[] = {KC_A,   KC_COMM,  COMBO_END};
 const uint16_t PROGMEM ani_combo_l[] = {KC_V,   KC_MINS,  COMBO_END};
 //                                          R:  -  -  X  X  -
 const uint16_t PROGMEM tab_combo_r[] = {KC_W,   KC_P,     COMBO_END};
@@ -140,28 +140,27 @@ combo_t key_combos[] = {
   /*   |XXX|   |___|   |___|   |XXX|   |___|               |___|   |XXX|   |___|   |___|   |XXX|   */
   [ALT_COMBO_L] = COMBO(alt_combo_l, KC_LALT),         [ALT_COMBO_R] = COMBO(alt_combo_r, KC_RALT),
   [CTL_COMBO_L] = COMBO(ctl_combo_l, KC_LCTL),         [CTL_COMBO_R] = COMBO(ctl_combo_r, KC_RCTL),
-  [G3D_COMBO_L] = COMBO(g3d_combo_l, TO(GM3D_LAYER)),  [MSE_COMBO_R] = COMBO(mse_combo_r, TO(MOUS_LAYER)),
+  [LNV_COMBO_L] = COMBO(lnv_combo_l, TO(LNAV_LAYER)),  [MSE_COMBO_R] = COMBO(mse_combo_r, TO(MOUS_LAYER)),
   /*   |___|   |___|   |___|   |XXX|   |XXX|               |XXX|   |XXX|   |___|   |___|   |___|   */
   [PSC_COMBO_L] = COMBO(psc_combo_l, KC_PSCR),         [PSC_COMBO_R] = COMBO(psc_combo_r, KC_PSCR),
   [ESC_COMBO_L] = COMBO(esc_combo_l, KC_ESC),          [ENT_COMBO_R] = COMBO(ent_combo_r, KC_ENT),
   [GUI_COMBO_L] = COMBO(gui_combo_l, KC_LGUI),         [GUI_COMBO_R] = COMBO(gui_combo_r, KC_RGUI),
   /*   |XXX|   |___|   |___|   |___|   |XXX|               |XXX|   |___|   |___|   |___|   |XXX|   */
   [KTP_COMBO_L] = COMBO(ktp_combo_l, KEY_TRAP),        [KTP_COMBO_R] = COMBO(ktp_combo_r, KEY_TRAP),
-  [G2D_COMBO_L] = COMBO(g2d_combo_l, TO(GM2D_LAYER)),  [NMB_COMBO_R] = COMBO(nmb_combo_r, TO(NMBR_LAYER)),
+  [GME_COMBO_L] = COMBO(gme_combo_l, TO(GAME_LAYER)),  [NMB_COMBO_R] = COMBO(nmb_combo_r, TO(NMBR_LAYER)),
   [ANI_COMBO_L] = COMBO(ani_combo_l, TO(ANIMAL_WELL_LOL)),
 
 };
 
 // Exclude some combos from game layers
 bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
-    if (layer_state_is(GM2D_LAYER) ||
-        layer_state_is(ANIMAL_WELL_LOL) ||
-        layer_state_is(GM3D_LAYER)) {
+    if (layer_state_is(GAME_LAYER) ||
+        layer_state_is(ANIMAL_WELL_LOL)) {
         switch (combo_index) {
             case ESC_COMBO_L:
             case ENT_COMBO_R:
-            case G2D_COMBO_L:
-            case G3D_COMBO_L:
+            case GME_COMBO_L:
+            case LNV_COMBO_L:
             case ALP_COMBO_R:
             case MSE_COMBO_R:
             case NMB_COMBO_R:
@@ -174,9 +173,8 @@ bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode
 
 // Shorten combo term when in game layers
 uint16_t get_combo_term(uint16_t index, combo_t *combo) {
-    if (layer_state_is(GM2D_LAYER) ||
-        layer_state_is(ANIMAL_WELL_LOL) ||
-        layer_state_is(GM3D_LAYER))
+    if (layer_state_is(GAME_LAYER) ||
+        layer_state_is(ANIMAL_WELL_LOL))
         return 6;
     return COMBO_TERM;
 }
@@ -184,7 +182,7 @@ uint16_t get_combo_term(uint16_t index, combo_t *combo) {
 // Switching to non-alpha layers breaks capsword
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-        case GM2D_LAYER ... MOUS_LAYER:
+        case GAME_LAYER ... MOUS_LAYER:
             caps_word_off(); break;
         default: break;
     }

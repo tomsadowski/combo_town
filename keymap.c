2c93c5e7d5e9ab99b572aaf4bd9b6921e1031562
tom <tsadowski93@gmail.com>
2024-06-16 22:10:36 -0400

    undid the dumb

--- keymap.c
+++ keymap.c
@@ -9,6 +9,7 @@
 #include "quantum_keycodes.h"
 
 #define MAX_HOLD 8
+#define GAME_COMBO_TERM 6
 
 enum my_keycodes {CAPS_ON = SAFE_RANGE, ALPHA_ON_CAPS_OFF, KEY_TRAP};
 typedef enum {CLOSED, HOLD, STANDBY, FREE} key_trap_state;
@@ -19,17 +20,23 @@ typedef struct {
     unsigned short size;
 }
 key_trap;
+
 static key_trap trap = {CLOSED, {0}, KC_NO, 0};
-bool process_trap(keyrecord_t*);
+static bool is_momentary_layer = false;
+
+bool process_key_trap(keyrecord_t*);
 bool trap_process_key(uint16_t, keyrecord_t*);
+bool process_alpha_on_caps_off(keyrecord_t*);
+bool process_caps_on(keyrecord_t*);
 
 enum layers {ALPHA_LAYER, GAME_LAYER, LNMB_LAYER, LMSE_LAYER, NMBR_LAYER, MOUS_LAYER,
              MO_ALP_LYR, MO_MAL_LYR, MO_MNM_LYR, MO_NMB_LYR, MO_MSE_LYR};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [ALPHA_LAYER] = LAYOUT_split_3x5_2( // ALPHA: Alphabet, punctuation, editing
         KC_X,    KC_C,    KC_H,    KC_B,    KC_Q,            KC_Z,    KC_Y,    KC_W,    KC_P,    KC_J,
         KC_A,    KC_S,    KC_R,    KC_T,    KC_COMMA,        KC_DOT,  KC_E,    KC_I,    KC_O,    KC_N,
-        KC_V,    KC_F,    KC_L,    KC_D,    KC_DEL,          KC_BSPC, KC_U,    KC_M,    KC_G,    KC_K,
+        KC_V,    KC_F,    KC_L,    KC_D,    KC_MINS,         KC_QUOT, KC_U,    KC_M,    KC_G,    KC_K,
                                   KC_BSPC, LT(MO_MSE_LYR, KC_SPC),       LT(MO_NMB_LYR, KC_SPC), KC_DEL),
     [GAME_LAYER] = LAYOUT_split_3x5_2( // GAME: Base sans combos and hold-tap features
         _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
@@ -38,23 +45,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                _______, KC_SPC,          KC_0,    _______),
     [LNMB_LAYER] = LAYOUT_split_3x5_2( // LEFT NUMBER
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______,         _______, KC_SCLN, KC_QUOT, KC_SLSH, KC_BSLS,
-        KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______,         _______, KC_MINS,  KC_EQL,  KC_LBRC, KC_RBRC,
+        KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______,         _______, KC_SCLN, KC_SLSH, KC_BSLS, KC_GRV,
+        KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______,         _______, KC_EQL,  KC_LBRC, KC_RBRC, _______,
                                                _______, KC_SPC,          KC_SPC,  _______),
     [LMSE_LAYER] = LAYOUT_split_3x5_2( // LEFT MOUSE
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-        KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, _______,         _______, KC_ACL0, KC_ACL1, KC_ACL2, KC_GRV,
-        KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, _______,         _______, KC_PSCR, KC_F11,  KC_F12,  KC_F13,
+        KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, _______,         _______, KC_ACL0, KC_ACL1, KC_ACL2, KC_PSCR,
+        KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, _______,         _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,
                                                KC_BTN2, KC_BTN1,         KC_SPC,  _______),
     [NMBR_LAYER] = LAYOUT_split_3x5_2( // NUMBER: digits, navigation keys, and symbols
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_BSLS, KC_SLSH, KC_QUOT, KC_SCLN, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-        KC_LBRC, KC_RBRC, KC_EQL,  KC_MINS, _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+        KC_GRV,  KC_BSLS, KC_SLSH, KC_SCLN, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+        _______, KC_LBRC, KC_RBRC, KC_EQL,  _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
                                                _______, _______,         LT(MO_ALP_LYR, KC_SPC),            _______),
     [MOUS_LAYER] = LAYOUT_split_3x5_2( // MOUSE: Mouse and function keys
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-        KC_GRV,  KC_ACL2, KC_ACL1, KC_ACL0, _______,         _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-        KC_F11,  KC_F12,  KC_F13,  KC_PSCR, _______,         _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
+        KC_PSCR, KC_ACL2, KC_ACL1, KC_ACL0, _______,         _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
+        KC_F11,  KC_F12,  KC_F13,  KC_F14,  _______,         _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
                                   _______, LT(MO_MAL_LYR, KC_SPC),       KC_BTN1, KC_BTN2),
     [MO_ALP_LYR] = LAYOUT_split_3x5_2( // MOMENTARY ALPHA: Accessed from NUMBER
         KC_X,   KC_C,     KC_H,    KC_B,    KC_Q,            KC_Z,    KC_Y,    KC_W,    KC_P,    KC_J,
@@ -68,18 +75,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                _______, _______,         LT(MO_MNM_LYR, KC_SPC),            KC_DEL),
     [MO_MNM_LYR] = LAYOUT_split_3x5_2( // MOMENTARY MOUSE NUMBER: accessed from MOMENTARY MOUSE ALPHA
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_BSLS, KC_SLSH, KC_QUOT, KC_SCLN, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-        KC_LBRC, KC_RBRC, KC_EQL,  KC_MINS,  _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+        KC_GRV,  KC_BSLS, KC_SLSH, KC_SCLN, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+        _______, KC_LBRC, KC_RBRC, KC_EQL,  _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
                                   _______, LT(MO_MAL_LYR, KC_SPC),       _______, _______),
     [MO_NMB_LYR] = LAYOUT_split_3x5_2( // MOMENTARY NUMBER: accessed from ALPHA
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_BSLS, KC_SLSH, KC_QUOT, KC_SCLN, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-        KC_LBRC, KC_RBRC, KC_EQL,  KC_MINS, _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+        KC_GRV,  KC_BSLS, KC_SLSH, KC_SCLN, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+        _______, KC_LBRC, KC_RBRC, KC_EQL,  _______,         _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
                                                _______, KC_SPC,          _______, _______),
     [MO_MSE_LYR] = LAYOUT_split_3x5_2( // MOMENTARY MOUSE: accessed from ALPHA
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-        KC_GRV,  KC_ACL2, KC_ACL1, KC_ACL0, _______,         _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-        KC_F11,  KC_F12,  KC_F13,  KC_PSCR, _______,         _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
+        KC_PSCR, KC_ACL2, KC_ACL1, KC_ACL0, _______,         _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
+        KC_F11,  KC_F12,  KC_F13,  KC_F14,  _______,         _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
                                                _______, _______,         KC_BTN1, KC_BTN2),
 };
 
@@ -109,7 +116,7 @@ const uint16_t PROGMEM alt_combo_l[] = {KC_X,   KC_B,     COMBO_END};
 const uint16_t PROGMEM ctl_combo_l[] = {KC_A,   KC_T,     COMBO_END};
 const uint16_t PROGMEM lms_combo_l[] = {KC_V,   KC_D,     COMBO_END};
 //                                          L:  -  -  -  X  X
-const uint16_t PROGMEM gui_combo_l[] = {KC_T,KC_X,     COMBO_END};
+const uint16_t PROGMEM gui_combo_l[] = {KC_T,   KC_X,     COMBO_END};
 const uint16_t PROGMEM esc_combo_l[] = {KC_B,   KC_COMMA, COMBO_END};
 //                                          L:  X  -  -  -  X
 const uint16_t PROGMEM ktp_combo_l[] = {KC_X,   KC_Q,     COMBO_END};
@@ -123,7 +130,7 @@ const uint16_t PROGMEM alt_combo_r[] = {KC_Y,   KC_J,     COMBO_END};
 const uint16_t PROGMEM ctl_combo_r[] = {KC_E,   KC_N,     COMBO_END};
 const uint16_t PROGMEM mse_combo_r[] = {KC_U,   KC_K,     COMBO_END};
 //                                          R:  X  X  -  -  -
-const uint16_t PROGMEM gui_combo_r[] = {KC_E, KC_J,     COMBO_END};
+const uint16_t PROGMEM gui_combo_r[] = {KC_E,   KC_J,     COMBO_END};
 const uint16_t PROGMEM ent_combo_r[] = {KC_DOT, KC_Y,     COMBO_END};
 //                                          R:  X  -  -  -  X
 const uint16_t PROGMEM ktp_combo_r[] = {KC_Z,   KC_J,     COMBO_END};
@@ -148,7 +155,7 @@ combo_t key_combos[] = {
 };
 
 // Exclude some combos from game layers
-bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
+bool combo_should_trigger(uint16_t combo_index, combo_t* combo, uint16_t keycode, keyrecord_t* record) {
     if (layer_state_is(GAME_LAYER)) {
         switch (combo_index) {
             case ESC_COMBO_L:
@@ -167,17 +174,25 @@ bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode
 }
 
 // Shorten combo term when in game layers
-uint16_t get_combo_term(uint16_t index, combo_t *combo) {
+uint16_t get_combo_term(uint16_t index, combo_t* combo) {
     if (layer_state_is(GAME_LAYER))
-        return 6;
+        return GAME_COMBO_TERM;
     return COMBO_TERM;
 }
 
 // Switching to non-alpha layers breaks capsword
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
+        case ALPHA_LAYER:
+            is_momentary_layer = false;
+            break;
         case GAME_LAYER ... MOUS_LAYER:
-            caps_word_off(); break;
+            is_momentary_layer = false;
+            caps_word_off();
+            break;
+        case MO_ALP_LYR ... MO_MSE_LYR:
+            is_momentary_layer = true;
+            break;
         default: break;
     }
   return state;
@@ -202,7 +217,7 @@ bool caps_word_press_user(uint16_t keycode) {
     }
 }
 // prevent user from accidentally triggering F keys
-bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+bool get_permissive_hold(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
         case LT(MO_MSE_LYR, KC_SPC):
             return true;
@@ -211,7 +226,7 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     }
 }
 // common symbols like apostrophe can be accessed without breaking flow
-bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
         case LT(MO_NMB_LYR, KC_SPC):
             return true;
@@ -220,29 +235,25 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     }
 }
 // Handle all the custom keys
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
-        case ALPHA_ON_CAPS_OFF:
-            caps_word_off();
-            layer_move(ALPHA_LAYER);
-            return true;
-
-        case KEY_TRAP:
-            return process_trap(record);
-
         case KC_A ... KC_RGUI:
+
             if (trap.state != CLOSED) return trap_process_key(keycode, record);
+
             return true;
 
+        case ALPHA_ON_CAPS_OFF:
+            return process_alpha_on_caps_off(record);
         case CAPS_ON:
-            caps_word_on();
-            return false;
+            return process_caps_on(record);
+        case KEY_TRAP:
+            return process_key_trap(record);
 
         default: return true;
     }
 }
-// Process KEY_TRAP event
-bool process_trap(keyrecord_t *record) {
+bool process_key_trap(keyrecord_t* record) {
     if (record->event.pressed)
         trap.state = HOLD;
     else if (trap.free_key == KC_NO)
@@ -251,18 +262,27 @@ bool process_trap(keyrecord_t *record) {
         trap.state = FREE;
     return false;
 }
-// Process basic keycode when trap is open
-bool trap_process_key(uint16_t keycode, keyrecord_t *record) {
+bool process_alpha_on_caps_off(keyrecord_t* record) {
     if (record->event.pressed) {
-        if (trap.state != FREE)
-            trap.free_key = keycode;
-        if (trap.state == STANDBY)
-            trap.state = FREE;
+        caps_word_off();
+        if (!is_momentary_layer) layer_move(ALPHA_LAYER);
+    }
+    return false;
+}
+bool process_caps_on(keyrecord_t* record) {
+    if (record->event.pressed)
+        caps_word_on();
+    return false;
+}
+// Process basic keycode when trap is open
+bool trap_process_key(uint16_t keycode, keyrecord_t* record) {
+    if (record->event.pressed) {
+        if (trap.state != FREE) trap.free_key = keycode;
+        if (trap.state == STANDBY) trap.state = FREE;
         return true;
     }
     if (trap.state == HOLD && trap.size < MAX_HOLD) {
-        if (keycode == trap.free_key)
-            trap.free_key = KC_NO;
+        if (trap.free_key == keycode) trap.free_key = KC_NO;
         trap.held_keys[trap.size] = keycode;
         trap.size++;
         return false;
